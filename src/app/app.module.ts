62bad00df6c699ca9dc739443982b42648f53b75
Bronson Avila <bronson.avila@gmail.com>
2018-09-10 14:59:18 -1000

    Configure "App" module to use "Header" and "UnauthHomePage" components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Components:
+import { HeaderComponent } from './components/header/header.component';
+
+// Pages:
+import { UnauthHomePageComponent } from './pages/unauth-home-page/unauth-home-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UnauthHomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: UnauthHomePageComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
