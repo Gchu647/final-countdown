9c5a2d24d4c66818ea6c65924ca6316c9a613456
Bronson Avila <bronson.avila@gmail.com>
2018-09-10 15:03:16 -1000

    Setup basic "unauth-home-page" component configuration

---
+++ src/app/pages/unauth-home-page/unauth-home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unauth-home-page',
+  templateUrl: './unauth-home-page.component.html',
+  styleUrls: ['./unauth-home-page.component.scss']
+})
+export class UnauthHomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
