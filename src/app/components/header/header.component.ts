9c2a3c022f38017313c6d505d85c93572d369e96
Bronson Avila <bronson.avila@gmail.com>
2018-09-10 20:54:54 -1000

    Add "header-auth-options" to "header" component (temp auth workaround)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  // Source (Stack Overflow): https://goo.gl/ozCQCt
+  // Used in displaying specific header links for authenticated users:
+  router: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private _router: Router
+  ) {
+    this.router = _router.url;
   }
-
 }
