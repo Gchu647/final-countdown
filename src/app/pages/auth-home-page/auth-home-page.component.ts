34e0de3002a904864f668919591ab20664df74d9
Bronson Avila <bronson.avila@gmail.com>
2018-09-10 20:57:11 -1000

    Setup basic "auth-home-page" component

---
+++ src/app/pages/auth-home-page/auth-home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-home-page',
+  templateUrl: './auth-home-page.component.html',
+  styleUrls: ['./auth-home-page.component.scss']
+})
+export class AuthHomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
