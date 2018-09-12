7697e2c12512521f830011e3d2a426caf77aca18
gchu <gchu647@gmail.com>
2018-09-11 14:30:17 -1000

    passport config in server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,83 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./routes');
 const PORT = process.env.PORT || 8080;
 
+// Auth Imports:
+const session = require('express-session');
+const Redis = require('connect-redis')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const User = require('./db/models/User');
+
 app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//   res.send('server.js smoke test!');
-// })
+// ---------------------=[   PASSPORT Config Start   ]=--------------------- //
+app.use(
+  session({
+    store: new Redis(),
+    secret: process.env.PASSPORT_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  return done(null, {
+    id: user.id,
+    username: user.username
+  });
+});
+
+passport.deserializeUser((user, done) => {
+  new User({ id: user.id })
+    .fetch()
+    .then(user => {
+      user = user.toJSON();
+      return done(null, {
+        // You can get more data from DB:
+        id: user.id,
+        username: user.username
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      return done(err);
+    });
+});
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    return new User({ username: username })
+      .fetch()
+      .then(user => {
+        if (!user) {
+          return done({ message: 'Wrong Username' });
+        } else {
+          user = user.toJSON();
+          bcrypt.compare(password, user.password).then(samePassword => {
+            if (samePassword) {
+              return done(null, user);
+            } else {
+              return done({ message: 'Wrong Password' });
+            }
+          });
+        }
+      })
+      .catch(err => {
+        console.log('error: ', err);
+        return done(err);
+      });
+  })
+);
+// ----------------------=[   PASSPORT Config End   ]=---------------------- //
 
 app.use('/api', routes);
 
