170dc66bbf0485fb21474a59d9ff6f2fb164b0c0
Bronson Avila <bronson.avila@gmail.com>
2018-09-12 16:27:10 -1000

    Add temporary variables to "recipients" component

--- src/app/pages/recipients/recipients.component.ts
+++ src/app/pages/recipients/recipients.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipients.component.scss']
 })
 export class RecipientsComponent implements OnInit {
+  // Temporary variables (until database integrated):
+  recipientIndividuals: object[] = [
+    { id: 1, first_name: 'Adam', last_name: 'Alpha' },
+    { id: 2, first_name: 'Ben', last_name: 'Beta' },
+    { id: 3, first_name: 'Greg', last_name: 'Gamma' }
+  ];
+  recipientGroups: object[] = [
+    { id: 1, name: 'Family' },
+    { id: 2, name: 'Friends' },
+    { id: 3, name: 'Haters' }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
