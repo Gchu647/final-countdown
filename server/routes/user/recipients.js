6e5fe084073e83c89fb06ceb88ab0160ff054460
gchu <gchu647@gmail.com>
2018-09-13 10:16:08 -1000

    can get all recipients by user

--- server/routes/user/recipients.js
+++ server/routes/user/recipients.js
@@ -4,7 +4,17 @@ const Recipient = require('../../db/models/Recipient');
 
 router.route('/:id/recipients')
   .get((req, res) => {
-    res.send('recipients smoke test!');
+    const userId = req.params.id;
+
+    return new Recipient()
+      .where({ 'sender_id': userId })
+      .fetchAll()
+      .then(recipients => {
+        return res.json(recipients);
+      })
+      .catch(err => {
+        return res.status(400).json({ message: err.message });
+      });
   })
 
 module.exports = router;
