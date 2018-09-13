56cb92f23f9ac31c80d2d5aeafcde7f6d7dff4ea
Bronson Avila <bronson.avila@gmail.com>
2018-09-13 10:25:35 -1000

    Modify "recipients" component to filter "relationships" (non-functional)

--- src/app/pages/recipients/recipients.component.ts
+++ src/app/pages/recipients/recipients.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipientsComponent implements OnInit {
   // Temporary variables (until database integrated):
-  recipientIndividuals: object[] = [
-    { id: 1, first_name: 'Adam', last_name: 'Alpha' },
-    { id: 2, first_name: 'Ben', last_name: 'Beta' },
-    { id: 3, first_name: 'Greg', last_name: 'Gamma' }
+  recipients: object[] = [
+    { id: 1, first_name: 'Adam', last_name: 'Alpha', relationshipId: 1 },
+    { id: 2, first_name: 'Ben', last_name: 'Beta', relationshipId: 2 },
+    { id: 3, first_name: 'Greg', last_name: 'Gamma', relationshipId: 3 }
   ];
-  recipientGroups: object[] = [
+  relationships: object[] = [
     { id: 1, name: 'Family' },
     { id: 2, name: 'Friends' },
     { id: 3, name: 'Haters' }
