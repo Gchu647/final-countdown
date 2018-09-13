1a77b3b976850651f15a3ecd8262c022185d9a54
gchu <gchu647@gmail.com>
2018-09-12 14:28:01 -1000

    changed profile to user route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('./auth');
-const profile = require('./profile');
+const user = require('./user');
 
 router.use('/', auth);
-router.use('/profile', profile);
+router.use('/user', user);
 
 module.exports = router;
