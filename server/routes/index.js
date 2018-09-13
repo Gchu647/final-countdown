afc5db558c46fe7a0581bf7dbd90167cb25ee6da
gchu <gchu647@gmail.com>
2018-09-12 15:00:26 -1000

    added relationships route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('./auth');
 const user = require('./user');
+const relationships = require('./relationships');
 
 router.use('/', auth);
 router.use('/user', user);
+router.use('/relationships', relationships);
 
 module.exports = router;
