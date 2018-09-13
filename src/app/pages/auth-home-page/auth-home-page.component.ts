f82450547fb197960d4c6ed70c14e9f45406cea2
Bronson Avila <bronson.avila@gmail.com>
2018-09-13 12:12:38 -1000

    Modify "auth-home-page" display of personal/group messages; sticky panic

--- src/app/pages/auth-home-page/auth-home-page.component.ts
+++ src/app/pages/auth-home-page/auth-home-page.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-home-page.component.scss']
 })
 export class AuthHomePageComponent implements OnInit {
+  // Temporary variables (until database integrated):
+  recipients: object[] = [
+    { id: 1, first_name: 'Adam', last_name: 'Alpha', relationshipId: 1 },
+    { id: 2, first_name: 'Ben', last_name: 'Beta', relationshipId: 2 },
+    { id: 3, first_name: 'Greg', last_name: 'Gamma', relationshipId: 3 }
+  ];
+  relationships: object[] = [
+    { id: 1, name: 'Family' },
+    { id: 2, name: 'Friends' },
+    { id: 3, name: 'Haters' }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
