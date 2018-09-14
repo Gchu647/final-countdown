19511489d77efd1cbeda4b4db4e33fca8b73b6ae
Bronson Avila <bronson.avila@gmail.com>
2018-09-13 19:34:24 -1000

    Configure "app" module to use "message-group" component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthHomePageComponent } from './pages/auth-home-page/auth-home-page.component';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { MessageNewComponent } from './pages/message-new/message-new.component';
+import { MessagePersonalNewComponent } from './pages/message-personal-new/message-personal-new.component';
+import { MessageGroupComponent } from './pages/message-group/message-group.component';
 import { RecipientsComponent } from './pages/recipients/recipients.component';
 import { RecipientNewComponent } from './pages/recipient-new/recipient-new.component';
 import { RecipientViewComponent } from './pages/recipient-view/recipient-view.component';
@@ -29,7 +30,8 @@ import { RecipientViewComponent } from './pages/recipient-view/recipient-view.co
     LoginComponent,
     AuthHomePageComponent,
     ProfileComponent,
-    MessageNewComponent,
+    MessagePersonalNewComponent,
+    MessageGroupComponent,
     RecipientsComponent,
     RecipientNewComponent,
     RecipientViewComponent
@@ -44,7 +46,8 @@ import { RecipientViewComponent } from './pages/recipient-view/recipient-view.co
       { path: 'login', component: LoginComponent },
       { path: 'home', component: AuthHomePageComponent },
       { path: 'profile', component: ProfileComponent },
-      { path: 'messages/new', component: MessageNewComponent },
+      { path: 'messages/personal/new', component: MessagePersonalNewComponent },
+      { path: 'messages/group/:id', component: MessageGroupComponent },
       { path: 'recipients', component: RecipientsComponent },
       { path: 'recipients/new', component: RecipientNewComponent },
       { path: 'recipients/:id', component: RecipientViewComponent }
@@ -53,4 +56,4 @@ import { RecipientViewComponent } from './pages/recipient-view/recipient-view.co
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
