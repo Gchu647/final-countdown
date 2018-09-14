1290c767f1df43ab233c347203d3a04b3aa4608f
gchu <gchu647@gmail.com>
2018-09-13 14:15:43 -1000

    added encryption_file seeds

--- server/db/seeds/08_add_packages.js
+++ server/db/seeds/08_add_packages.js
@@ -5,5 +5,8 @@ exports.seed = function(knex, Promise) {
     {id: 1, package_maker_id: 1, recipient_id: 1},
     {id: 2, package_maker_id: 1, recipient_id: 2},
     {id: 3, package_maker_id: 1, recipient_id: 3},
+    {id: 4, package_maker_id: 2, recipient_id: 4},
+    {id: 5, package_maker_id: 2, recipient_id: 5},
+    {id: 6, package_maker_id: 2, recipient_id: 6},
   ]);
 };
