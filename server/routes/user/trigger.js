c86b576491cc2fa2251946adc2b76439536eca5d
Bronson Avila <bronson.avila@gmail.com>
2018-09-17 19:20:52 -1000

    Add route to GET triggers; modify data processing re: POST triggers

--- server/routes/user/trigger.js
+++ server/routes/user/trigger.js
@@ -1,68 +1,80 @@
-// This routes lets user post a trigger or flags a trigger
+// This route allows a user to activate/deactivate the user's countdown trigger:
 const express = require('express');
 const router = express.Router();
+
 const isAuthenticated = require('../../middleware/isAuthenticated');
 const Trigger = require('../../db/models/Trigger');
 
-// Posting and flagging user triggers
 router.route('/:id/trigger')
-  .post(isAuthenticated, (req, res) => {
+  .get(isAuthenticated, (req, res) => {
     const userId = req.params.id;
-    const customTimer = req.body.customTimer; // Currently in seconds
-    let time = new Date();
 
-    // set time with customerTimer or default
-    if(customTimer) {
-      time.setSeconds(time.getSeconds() + customTimer);
-    } else {
-      time.setSeconds(time.getSeconds() + 604800); // 7 days
-    }
+    return new Trigger()
+      .query(qb => {
+        qb.whereNot({ countdown: null }).andWhere({ user_id: userId });
+      })
+      .fetch()
+      .then(trigger => {
+        return res.json(trigger);
+      })
+      .catch(err => {
+        return res.status(400).json({ error: err.message });
+      });
+  })
+  .post(isAuthenticated, (req, res) => {
+    const userId = req.query.id;
+    const countdownDays = Number(req.query.countdownDays);
+    const time = new Date();
 
-    let triggerInput = {
+    // 86,400 seconds in a day (LOWER THIS VALUE WHEN TESTING):
+    time.setSeconds(time.getSeconds() + (countdownDays * 86400));
+
+    const triggerInput = {
       user_id: userId ? userId.trim() : null,
-      countdown: time.toUTCString(),
-    }
+      countdown: time.toUTCString()
+    };
 
-    // Checks if all old triggers are deleted before creating a new one
+    // Check if all old triggers are deleted before creating a new one:
     new Trigger()
-    .query(qb => { // First check if all old triggers are deleted
-      qb.whereNot({ countdown: null })
-        .andWhere({ user_id: userId });
-    })
-    .fetch()
-    .then( trigger => {
-      if(trigger !== null) {
-        throw new Error('You already set up a trigger!');
-      } else {
-        // Save trigger if all old triggers are deleted
-        return new Trigger()
-        .save(triggerInput)
-        .then(trigger => {
-          return res.json(trigger);
-        })
-      }
-    })
-    .catch(err => {
-      return res.status(400).json({ error: err.message});
-    });
-
+      .query(qb => {
+        // Check if all old triggers are deleted:
+        qb.whereNot({ countdown: null }).andWhere({ user_id: userId });
+      })
+      .fetch()
+      .then(trigger => {
+        if (trigger !== null) {
+          throw new Error('You already set up a trigger!');
+        } else {
+          // Save trigger if all old triggers are deleted:
+          return new Trigger().save(triggerInput).then(trigger => {
+            return res.json(trigger);
+          });
+        }
+      })
+      .catch(err => {
+        return res.status(400).json({ error: err.message });
+      });
   })
   .delete(isAuthenticated, (req, res) => {
-   const userId = req.params.id;;
+    const userId = req.params.id;
 
-    // flags the trigger input
+    // Flag active trigger as being "deleted":
     return new Trigger()
-    .where({'user_id': userId})
-    .save({
-      countdown: null
-    }, { patch: true })
-    .then(trigger => {
-      return res.json(trigger);
-    })
-    .catch(err => {
-      return res.status(400).json({ error: err.message });
-    });
-
-  })
+      .query(qb => {
+        qb.whereNot({ countdown: null }).andWhere({ user_id: userId });
+      })
+      .save(
+        {
+          countdown: null
+        },
+        { patch: true }
+      )
+      .then(trigger => {
+        return res.json(trigger);
+      })
+      .catch(err => {
+        return res.status(400).json({ error: err.message });
+      });
+  });
 
 module.exports = router;
