6df0a73aaef070b6cf32a13296384fe5b8d9ff97
gchu <gchu647@gmail.com>
2018-09-17 14:42:27 -1000

    fetch relationships in recipient view

--- src/app/pages/recipient-view/recipient-view.component.ts
+++ src/app/pages/recipient-view/recipient-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   selector: 'app-recipient-view',
@@ -8,22 +9,35 @@ import { AuthService } from '../../services/auth.service';
 })
 export class RecipientViewComponent implements OnInit {
   formData: object;
-  relationships: object[] = [
-    { id: 1, name: 'Family' },
-    { id: 2, name: 'Friend' },
-    { id: 3, name: 'Hater' }
-  ];
+  relationships: object[];
   message: string =
     'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum facilis, ipsa ipsam unde, veniam assumenda iste saepe cumque similique tenetur provident perspiciatis rem harum. Incidunt explicabo perspiciatis alias quis ipsa!';
 
-  constructor(private auth: AuthService) {}
+  constructor(
+    private backend: BackendService,
+    private auth: AuthService
+  ) {}
 
   ngOnInit() {
+    // Get relationships from server and capitalize first letter of each:
+    this.backend.fetchRelationships().then((response: object[]) => {
+      const capitalizedRelationships = response.map(relationship => {
+        const capitalizedRelationship = Object.assign(relationship);
+        capitalizedRelationship['name'] =
+          capitalizedRelationship.name.charAt(0).toUpperCase() +
+          capitalizedRelationship.name.substr(1);
+        return capitalizedRelationship;
+      });
+
+      this.relationships = capitalizedRelationships;
+    });
+    
+    
     // Getting the recipientId from URL
     let index = window.location.pathname.length -1;
     let recipientId = Number(window.location.pathname.charAt(index));
-    console.log('recipientId: ', recipientId);
-
+    
+    // Getting the recipient info using recipientId
     this.auth.fetchRecpientById(recipientId)
     .then((response: object) => {
       this.formData = response;
