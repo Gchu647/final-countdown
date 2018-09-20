96cefbddf1b3fca85e700856b1e365515b2319d2
gchu <gchu647@gmail.com>
2018-09-20 10:32:57 -1000

    made a delete recipient button

--- src/app/pages/recipient-view/recipient-view.component.ts
+++ src/app/pages/recipient-view/recipient-view.component.ts
@@ -118,6 +118,10 @@ export class RecipientViewComponent implements OnInit {
     });
   }
 
+  deleteRecipient() {
+    console.log('deleted a recipient!');
+  }
+
   // ------------------------------------------------------------------------ //
 
   validateName(classNameStr) {
